33e6742ddf12f2fe694280b7c99a3b10a7490e30
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2025-12-18 23:18:36 -0600

    Add log in routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,20 @@
 import express from 'express';
-import { getSignUpHandler, postSignUpHandler } from '../controllers/auth.js';
+import {
+  getLogInHandler,
+  getSignUpHandler,
+  postLogInHandler,
+  postSignUpHandler,
+} from '../controllers/auth.js';
 import signUpValidator from '../middleware/validators/signUpValidator.js';
 import validate from '../middleware/validate.js';
+import logInValidator from '../middleware/validators/logInValidator.js';
 
 const router = express.Router();
 
 router.get('/signup', getSignUpHandler);
 router.post('/signup', signUpValidator, validate, postSignUpHandler);
+
+router.get('/login', getLogInHandler);
+router.post('/login', logInValidator, postLogInHandler);
 
 export default router;

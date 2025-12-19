556c71533b4b742f842cb2e97cc93777c9863d85
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2025-12-18 23:21:19 -0600

    Add passport and session config to app

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 import { join } from 'node:path';
 import express from 'express';
 import router from './routes/index.js';
+import passport from 'passport';
+import session from 'express-session';
+import pool from './config/database.js';
+import connectPgSimple from 'connect-pg-simple';
+import './config/paspport.js';
 
 const app = express();
 app.set('views', join(import.meta.dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(join(import.meta.dirname, 'public')));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const PGStore = connectPgSimple(session);
+
+const sessionStore = new PGStore({
+  pool: pool,
+});
+
+app.use(
+  session({
+    store: sessionStore,
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 1 day
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', router);
 

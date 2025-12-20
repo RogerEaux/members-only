cc237fa2fe595f681133410114362cbd50516c7c
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2025-12-19 20:54:05 -0600

    Remove validate middleware in favour of controller specific validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,29 @@
 import passport from 'passport';
 import { createUser } from '../models/userModel.js';
 import bcrypt from 'bcrypt';
+import { validationResult } from 'express-validator';
 
 export function getSignUpHandler(req, res, next) {
   res.render('signup', { errors: {}, values: {} });
 }
 
 export async function postSignUpHandler(req, res, next) {
-  if (req.errors) {
-    const errors = req.errors.reduce((acc, curr) => {
-      acc[curr.path] = curr.msg;
+  const errors = validationResult(req);
 
-      return acc;
-    }, {});
-
-    res.render('signup', { errors, values: req.body });
-  } else {
-    const { username, email, password } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    const user = { username, email, hash };
-
-    const createdUser = (await createUser(user)).rows[0];
-
-    res.send(createdUser);
+  if (!errors.isEmpty()) {
+    return res.render('signup', {
+      errors: errors.mapped(),
+      values: req.body,
+    });
   }
+
+  const { username, email, password } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = { username, email, hash };
+
+  const createdUser = (await createUser(user)).rows[0];
+
+  res.send(createdUser);
 }
 
 export function getLogInHandler(req, res, next) {
--- routes/auth.js
+++ routes/auth.js
@@ -6,13 +6,12 @@ import {
   postSignUpHandler,
 } from '../controllers/auth.js';
 import signUpValidator from '../middleware/validators/signUpValidator.js';
-import validate from '../middleware/validate.js';
 import logInValidator from '../middleware/validators/logInValidator.js';
 
 const router = express.Router();
 
 router.get('/signup', getSignUpHandler);
-router.post('/signup', signUpValidator, validate, postSignUpHandler);
+router.post('/signup', signUpValidator, postSignUpHandler);
 
 router.get('/login', getLogInHandler);
 router.post('/login', logInValidator, postLogInHandler);
